83e30fae59f6c22b765b09e37e56f8e02d64cccb
Ismael Perez <ismael.perez8411@gmail.com>
2022-01-23 01:01:02 -0500

    setting projectID for firebase on github deploy flow

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "@firebase/app";
 import { getFirestore } from "firebase/firestore";
-//import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 import { getStorage } from "firebase/storage";
